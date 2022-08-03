b418baf3224cde0938af77d34007188d519d4238
yoojin <yoojin9005@gmail.com>
2022-08-03 17:16:42 -0400

    navbar styling

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,22 +1,27 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
-import Navigation, {NavWrapper} from './Styles/Navigation.style'
+import {Navigation, NavWrapper} from './Styles/Navigation.style'
+import {RiSpotifyFill} from 'react-icons/ri'
+import {MdHome, MdOutlineHome} from 'react-icons/md'
 
 function NavBar() {
   return (
     <NavWrapper>
-      <Navigation as={NavLink} exact to='/'>
-        Home
-      </Navigation>
-      <Navigation as={NavLink} exact to='/library'>
-        Library
-      </Navigation>
-      <Navigation as={NavLink} exact to='/search'>
-        Search
-      </Navigation>
-      <Navigation as={NavLink} exact to='/for-artists'>
-        For Artists
-      </Navigation>
+      <div className='logo'>
+        <RiSpotifyFill color='white'/>
+      </div>
+        <Navigation as={NavLink} exact to='/'>
+          Home
+        </Navigation>
+        <Navigation as={NavLink} exact to='/library'>
+          Library
+        </Navigation>
+        <Navigation as={NavLink} exact to='/search'>
+          Search
+        </Navigation>
+        <Navigation as={NavLink} exact to='/for-artists'>
+          For Artists
+        </Navigation>
     </NavWrapper>
   )
 }
